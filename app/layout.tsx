519c708230a9b7c2569ee415d14fff9962b26483
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-14 21:45:15 -0300

    fix icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Image Restoration",
-  icons: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fbr%2Ficone-gratis%2Fperfil_4645949&psig=AOvVaw09DzpiHqZHPN_r40ZWUuA1&ust=1709945666177000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiMhttps://i.pinimg.com/564x/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.jpg75G644QDFQAAAAAdAAAAABAE",
-  
+  title: "AI Image Future",
 };
 
 export default function RootLayout({
@@ -19,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
         <Head>
-        <link rel="icon" href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.com%2Fbr%2Ficone-gratis%2Fperfil_4645949&psig=AOvVaw09DzpiHqZHPN_r40ZWUuA1&ust=1709945666177000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiM75G644QDFQAAAAAdAAAAABAE" />
+        <link rel="icon" href="https://th.bing.com/th/id/R.f725811e5ed26253868a1db03976d00d?rik=rfyUrfzeX3Ht2w&pid=ImgRaw&r=0" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
